c85b0d1075b47cfda5f5721a118baa04501e2cf0
Sagar9081 <83939401+Sagar9081@users.noreply.github.com>
2021-09-12 20:42:40 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./sb-admin2.css";
+import Sb2 from "./app";
+
+ReactDOM.render(
+  <>
+    <Sb2 />
+  </>,
+  document.getElementById("root")
+);
